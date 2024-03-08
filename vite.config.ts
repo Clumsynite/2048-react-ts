3deaf775c8f8b9e0900a006cd8ce2d639eceee96
Rishabh Pathak <clusmsyknight@gmail.com>
2024-03-08 17:31:22 +0530

    fixed build #deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  define: {
+    global: {},
+  },
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: (id: string) => {
+          const packages = id.split("node_modules");
+          if (packages.length > 1) {
+            return packages[1].split("/")[1];
+          }
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
+  base: "/2048-react-ts",
+});
