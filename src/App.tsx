863669240c389cb18f08edafa7c80a3f42a91ae9
Rishabh Pathak <clusmsyknight@gmail.com>
2024-03-08 17:26:28 +0530

    fixed heading

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Board from "./components/Board";
 function App() {
   return (
     <div className="max-w-screen min-h-screen text-foreground bg-foreground transition-all p-12">
-      <div className="p-4 text-center font-bold text-2xl">2048</div>
+      <div className="p-4 text-center font-bold text-8xl">2048</div>
       <Board />
     </div>
   );
